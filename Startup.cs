1250ff3f7da282495a1d4fe0e82c9d800aa65c4b
Hadikusumo <john.hadikusumo@gmail.com>
2014-10-31 07:25:40 +1100

    AngularJS---Simple Starting Project
    My First Angular Project. Simple one.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJS__Member_Registration.Startup))]
+namespace AngularJS__Member_Registration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
